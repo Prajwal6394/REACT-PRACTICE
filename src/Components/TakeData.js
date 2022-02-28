4c70bb23b893dc0bdcf47075fd44026ce437045c
Prajwal Pandey <prajwalpandey2009@gmail.com>
2022-02-28 15:24:07 +0530

    commit

--- src/Components/TakeData.js
+++ src/Components/TakeData.js
@@ -4,16 +4,14 @@ import db from "../firebase";
 
 const TakeData = () => {
   const [name, setname] = useState("");
-  const [mobile, Setnum] = useState("");
-  const [college, setcollege] = useState("");
+  // const [mobile, Setnum] = useState("");
+  // const [college, setcollege] = useState("");
   const sub = (e) => {
     e.preventDefault();
 
     db.collection("data")
       .add({
-        Name: name,
-        MobileNum: number,
-        College: collegename
+        Name: name
       })
 
       .then((docRef) => {
@@ -31,13 +29,13 @@ const TakeData = () => {
           <input type = "text" placeholder="Prajwal" onChange={(e) =>{setname(e.target.value)}} />
           <br/>
           <br/>
-          <input type= "number" placeholder="926242265" onChange={(e) => {Setnum(e.target.value)}}/>
+          {/* <input type= "number" placeholder="926242265" onChange={(e) => {Setnum(e.target.value)}}/>
           <br/>
           <br/>
 
           <input type = "text" placeholder="BBDNIIT" onChange={(e) => {setcollege(e.target.value)}}/>
           <br/>
-          <br/>
+          <br/> */}
           <button type = "submit">Submit</button>
         </form>
       </center>
